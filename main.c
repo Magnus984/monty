3c2c15cb33da74a8ff09b2e978d75645be756673
Jesse Ntow <Jessentow@gmail.com>
2024-01-18 15:17:43 -0800

    monty

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
 #include "monty.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 char *line = NULL;
 /**
@@ -34,27 +31,3 @@ int main(int argc, char **argv)
 	return (0);
 
 }
-/**
- * error_usage - This will print usage message and exit
- *
- * Return: nothing
- */
-void error_usage(void)
-{
-	fprintf(stderr, "USAGE: monty file\n");
-	exit(EXIT_FAILURE);
-}
-
-/**
- * file_error - This will print file error message and exit
- * @argv: argv given by the  manin
- *
- * Return: nothing
- */
-
-void file_error(char *argv)
-{
-	fprintf(stderr, "Error: Can't open file %s\n", argv);
-	exit(EXIT_FAILURE);
-}
-
